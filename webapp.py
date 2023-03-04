28b77896850032e4dc96342d7fe314e0a83917fd
AGuyWithANose <48656722+AGuyWithANose@users.noreply.github.com>
2023-03-04 15:19:05 -0500

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -2,7 +2,15 @@ import streamlit as st
 
 st.set_page_config(page_title="Shady n", page_icon=":tada:", layout="wide")
 
-st.subheader("I Love Big Black Men :wave:")
-st.title("I like dudes")
-st.write("I am baller")
-st.write("[Like men too?](https://www.youtube.com/watch?v=VqgUkExPvLY)")
+with st.container();
+  st.subheader("I Love Big Black Men :wave:")
+  st.title("I like dudes")
+  "I am baller"
+  st.write("[Like men too?](https://www.youtube.com/watch?v=VqgUkExPvLY)")
+
+with st.container();
+  "Zangbus"
+  left_column, right_column = st.columns(2)
+  with left_column:
+    st.header("What I Do")
+    "Ballingus"
