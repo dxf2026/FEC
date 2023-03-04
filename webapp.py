09c3ce8bd47958fc26f0f2ef86577e54016716e9
AGuyWithANose <48656722+AGuyWithANose@users.noreply.github.com>
2023-03-04 15:15:38 -0500

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+st.set_page_config(page_title="Shady n", page_icon=":tada:", layout="wide")
+
+st.subheader("I Love Big Black Men :wave:")
+st.title("I like dudes")
+st.write("I am baller")
+st.write("[Like men too?](https://www.youtube.com/watch?v=VqgUkExPvLY)")
