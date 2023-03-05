79e2be5ded076312a1f7bf2b076e0e8e495d5239
dxf2026 <126975969+dxf2026@users.noreply.github.com>
2023-03-05 03:02:15 -0500

    Update speechrecog.py

--- speechrecog.py
+++ speechrecog.py
@@ -1,22 +1,36 @@
 from nltk.sentiment import SentimentIntensityAnalyzer
 import nltk
 import moviepy.editor as mp
+import speech_recognition as sr
+
 nltk.download('vader_lexicon')
 
-def toMp3(mp4inp, mp3dest):
-    video = mp.VideoFileClip("C:\\Users\\dxf20\\PycharmProjects\\HackTJ\\FileSaving\\video.mp4")
+def toMP3():
+    video = mp.VideoFileClip("FileSaving\\test.mp4")
     audio = video.audio
-    audio.write_audiofile("C:\\Users\\dxf20\\PycharmProjects\\HackTJ\\FileSaving\\sound.mp3")
+    audio.write_audiofile("FileSaving\\sound.wav")
 
 def getSentiment(text):
     nia = SentimentIntensityAnalyzer()
     return nia.polarity_scores(text)
 
-# def main():
-#     inp = "C:\\Users\\dxf20\\PycharmProjects\\HackTJ\\FileSaving\\video.mp4"
-#     out = "C:\\Users\\dxf20\\PycharmProjects\\HackTJ\\FileSaving\\sound.mp4"
+def generate():
+    inp = "FileSaving\\video.mp4"
+    out = "FileSaving\\sound.wav"
 
-#     toMp3(inp, out)
+    toMP3()
+    # engine = tts.init()
+    # engine.save_to_file('''We can discuss your product design later. Email me or something. Please do not make any inquiries about your design right now.''', out)
+    # engine.runAndWait()
 
-# if __name__ == "__main__":
-#     main()
+    return(out)
+
+
+def get():
+    engine = sr.Recognizer()
+    mp3FileName = generate()
+    with sr.AudioFile(mp3FileName) as source:
+        audio = engine.record(source)
+
+    text = engine.recognize_google(audio)
+    return(getSentiment(text))
