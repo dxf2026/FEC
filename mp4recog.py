66be1851cd783bb929c629a31e8863ac4775cb21
ghuang2026 <126977915+ghuang2026@users.noreply.github.com>
2023-03-05 06:05:25 -0500

    Update mp4recog.py

--- mp4recog.py
+++ mp4recog.py
@@ -1,38 +1,20 @@
 from deepface import DeepFace
 import cv2
 def checkFrame(path):
-    face_analysis = DeepFace.analyze(img_path=path, actions=['emotion'])
-    return(face_analysis[0]['emotion'])
-
-# Program To Read video
-# and Extract Frames
-
-import cv2
+	face_analysis = DeepFace.analyze(img_path=path, actions=['emotion'])
+	return(face_analysis[0]['emotion'])
 
 # Function to extract frames
 def FrameCapture():
 
 	# Path to video file
 	vidObj = cv2.VideoCapture("\\FileSaving\\test.mp4")
+	totalFrames = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
 
-	# Used as counter variable
-	count = 0
-
-	# checks whether frames were extracted
-	success = 1
-    average = {'angry': 0, 'disgust': 0, 'fear': 0, 'happy': 0, 'sad': 0, 'suprise' : 0, 'neutral' : 0}
-	while success:
-
-		# vidObj object calls read
-		# function extract frames
-		success, image = vidObj.read()
-
-		# Saves the frames with frame-count
-		cv2.imwrite("\\FileSaving\\frame.jpg", image)
-
-        temp = checkFrame("\\Frames\\frame.jpg")
-        for key in temp.keys():
-            average[key] += temp[key]
-
-    r = {"neg" : (average['disgust']+average['angry']+average['fear'])/count, 'neu' : (average['suprise']+average['neutral'])/2, 'pos' : (average["happy"]/count)}
-    return r
+	randomFrameNumber=random.randint(0, totalFrames)
+	vidcap.set(cv2.CAP_PROP_POS_FRAMES,randomFrameNumber)
+	success, image = vidcap.read()
+	if success:
+    		cv2.imwrite("\\FileSaving\\random_frame.jpg", image)
+	
+	return(checkFrame("\\FileSaving\\random_frame.jpg))
