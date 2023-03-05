172e70008bc9dab217bdb8ebe7aede6c2bb2fc37
ghuang2026 <126977915+ghuang2026@users.noreply.github.com>
2023-03-04 21:04:47 -0500

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from PIL import Image
 
-with open("style.css") as t:
-  st.markdown(t'<style>(t.read*())</style>', unsafe_allow_html=True)
+with open("style.css") as f:
+  st.markdown(f'<style>(f.read*())</style>', unsafe_allow_html=True)
 st.set_page_config(page_title="Divinity", page_icon="logo.png", layout="wide")
 st.title("Dectecting emotion from sound")
 st.subheader("Upload Sound File")
