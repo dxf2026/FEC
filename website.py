4565bbca361122ded719f0dfacbccc6fa3450f2f
AGuyWithANose <48656722+AGuyWithANose@users.noreply.github.com>
2023-03-05 04:33:37 -0500

    Update website.py

--- website.py
+++ website.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from PIL import Image
-import moviepy as mp
+import moviepy.editor as mp
 
 st.set_page_config(page_title="FEC", page_icon=":detective:", layout="wide")
 if st.sidebar.button("Home"):
