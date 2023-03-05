dc3aee6ad0bfc902096040185da20402edb47e4b
ghuang2026 <126977915+ghuang2026@users.noreply.github.com>
2023-03-05 05:36:01 -0500

    Update mp4recog.py

--- mp4recog.py
+++ mp4recog.py
@@ -4,24 +4,35 @@ def checkFrame(path):
     face_analysis = DeepFace.analyze(img_path=path, actions=['emotion'])
     return(face_analysis[0]['emotion'])
 
-def checkFrames(mp4):
-    vidcap = cv2.VideoCapture('\\FileSaving\\test.mp4')
-    success, image = vidcap.read()
-    count = 0
-    success = True
-    while success:
-        success, image = vidcap.read()
-        cv2.imwrite("\\Frames\\frame"+ str(count), image)  # save frame as JPEG file
-        if cv2.waitKey(10) == 27:  # exit if Escape is hit
-            break
-        count += 1
+# Program To Read video
+# and Extract Frames
 
+import cv2
+
+# Function to extract frames
+def FrameCapture():
+
+	# Path to video file
+	vidObj = cv2.VideoCapture("\\FileSaving\\test.mp4")
+
+	# Used as counter variable
+	count = 0
+
+	# checks whether frames were extracted
+	success = 1
     average = {'angry': 0, 'disgust': 0, 'fear': 0, 'happy': 0, 'sad': 0, 'suprise' : 0, 'neutral' : 0}
-    for i in range(count-1):
-        temp = checkFrame("\\Frames\\frame" + str(i))
+	while success:
+
+		# vidObj object calls read
+		# function extract frames
+		success, image = vidObj.read()
+
+		# Saves the frames with frame-count
+		cv2.imwrite("\\FileSaving\\frame.jpg", image)
+
+        temp = checkFrame("\\Frames\\frame.jpg")
         for key in temp.keys():
             average[key] += temp[key]
 
     r = {"neg" : (average['disgust']+average['angry']+average['fear'])/count, 'neu' : (average['suprise']+average['neutral'])/2, 'pos' : (average["happy"]/count)}
     return r
-
