cb39f38eb00ea45cd6fdace3dda1e7f080b786bf
AGuyWithANose <48656722+AGuyWithANose@users.noreply.github.com>
2023-03-04 19:01:42 -0500

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -4,22 +4,23 @@ from PIL import Image
 st.set_page_config(page_title="Divinity", page_icon="logo.png", layout="wide")
 
 image_file = st.file_uploader("Upload mp4 File", type=["mp4"])
-
-if st.button("Calculate emotions"):
-  with st.container():
-    st.subheader("I Love Big Black Men :wave:")
-    st.title("I like dudes")
-    "I am baller"
-    st.write("[Like men too?](https://www.youtube.com/watch?v=VqgUkExPvLY)")
-
-if st.button("Calculate tone"):
-  st.write("Grippy")
-  with st.container():
-    "Zangbus"
-    left_column, right_column = st.columns(2)
-  with left_column:
-    st.header("What I Do")
-    "Ballingus"
-  with right_column:
-    st.header("man liker")
-    "Gugga"
+l_column, r_column = st.columns(2)
+with l_column:
+  if st.button("Calculate emotions"):
+    with st.container():
+      st.subheader("I Love Big Black Men :wave:")
+      st.title("I like dudes")
+      "I am baller"
+      st.write("[Like men too?](https://www.youtube.com/watch?v=VqgUkExPvLY)")
+with r_column:
+  if st.button("Calculate tone"):
+    st.write("Grippy")
+    with st.container():
+      "Zangbus"
+      left_column, right_column = st.columns(2)
+    with left_column:
+      st.header("What I Do")
+      "Ballingus"
+    with right_column:
+      st.header("man liker")
+      "Gugga"
